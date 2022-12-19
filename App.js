a5cf208037e24f9001d6fdc9b910fde5fddb451e
Chris Thompson <christhompsondev@protonmail.com>
2022-12-19 10:11:47 -0500

    added AWS login and settings screen

--- App.js
+++ App.js
@@ -3,7 +3,13 @@ import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './src/navigation';
 import ChatsScreen from './src/screens/ChatsScreen';
 
-export default function App() {
+import { Amplify } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react-native';
+import awsconfig from './src/aws-exports'
+
+Amplify.configure({...awsconfig, Analytics: { disabled: true } })
+
+function App() {
   return (
     <View style={styles.container}>
       <Navigator />
@@ -20,3 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',    
   },
 });
+
+export default withAuthenticator(App);
